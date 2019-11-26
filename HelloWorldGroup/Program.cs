03532f8d15d0bd62331329677129eb6b5306202f
Kimon Kremyzas <kimo0016@edu.easj.dk>
2019-11-26 11:21:38 +0100

    greek products

--- HelloWorldGroup/Program.cs
+++ HelloWorldGroup/Program.cs
@@ -13,7 +13,7 @@ namespace HelloWorldGroup
             Console.WriteLine("Hello Universe!");
             Console.WriteLine("Hello fellas");
             Console.WriteLine("Whazaaap");
-            Console.WriteLine();
+            Console.WriteLine("Φέτα, σουβλάκι, τζατζίκι");
         }
     }
 }
